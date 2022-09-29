06f307ae1cfc6354dfc80e6b8f18e9f3e3156d2f
Satyavathi-Seelam1 <sathyavathi.seelam@mutualmobile.com>
2022-09-29 19:40:16 +0530

    Food Screen new Activity created

---
+++ app/src/main/java/com/mutualmobile/swiggy_clone/FoodTabActivity.kt
@@ -0,0 +1,30 @@
+package com.mutualmobile.swiggy_clone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.mutualmobile.swiggy_clone.ui.theme.SwiggyCloneTheme
+import com.mutualmobile.swiggy_clone.viewmodels.FoodScreenViewModel
+
+class FoodTabActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      SwiggyCloneTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.background(color = Color.White)) {
+          Column {
+            val foodScreenViewModel: FoodScreenViewModel by viewModels()
+            FoodScreen(foodScreenViewModel)
+          }
+        }
+      }
+    }
+  }
+}
